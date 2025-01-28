12ecdfa28b6934fb49e84a35810e9b19feacff1a
Shaxriyor <shaxriyorturgunboyovshaxriyor@gmail.com>
2025-01-28 20:38:09 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
 import { GrCentos } from "react-icons/gr";
 import { TbSettingsStar } from "react-icons/tb";
 import { FcBullish } from "react-icons/fc";
 import { GrLike } from "react-icons/gr";
-import { GiFocusedLightning } from "react-icons/gi";
-
-
+import { CiCloudSun } from "react-icons/ci";
+import { CiTablets1 } from "react-icons/ci";
 
 function App() {
   return (
@@ -13,11 +16,11 @@ function App() {
       <section className="Section-1">
         <div className="container">
           <p className="text">
-            <GrCentos /> Sitemark
+            <GrCentos className="ll" /> Sitemark
           </p>
           <div className="hero">
             <div>
-              <TbSettingsStar />
+              <TbSettingsStar className="ff" />
             </div>
             <div>
               <p className="text-ls">Adaptable performance</p>
@@ -29,7 +32,7 @@ function App() {
           </div>
           <div className="hero">
             <div>
-              <FcBullish />
+              <FcBullish className="ff" />
             </div>
             <div>
               <p className="text-ls">Built to last</p>
@@ -39,20 +42,21 @@ function App() {
               </p>
             </div>
           </div>
-             <div className="hero">
+          <div className="hero">
             <div>
-            <GiFocusedLightning />
+              <CiCloudSun className="ff" />
             </div>
             <div>
               <p className="text-ls">Innovative functionality</p>
               <p className="text-lg">
-              Stay ahead with features that set new standards, addressing your evolving needs better than the rest.
+                Stay ahead with features that set new standards, addressing your
+                evolving needs better than the rest.
               </p>
             </div>
           </div>
           <div className="hero">
             <div>
-            <GrLike />
+              <CiTablets1 className="ff" />
             </div>
             <div>
               <p className="text-ls">Adaptable performance</p>
@@ -62,7 +66,47 @@ function App() {
               </p>
             </div>
           </div>
-       
+        </div>
+        <div className="box-input">
+          <p className="text-k">Sign in</p>
+          <Box
+            component="form"
+            sx={{ "& .MuiTextField-root": { m: 1, width: "100%" } }}
+            noValidate
+            autoComplete="off"
+          >
+            <div>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  "& > :not(style)": { m: 1 },
+                }}
+              >
+                <TextField
+                  helperText="Please enter your name"
+                  id="demo-helper-text-misaligned"
+                  label="Name"
+                />
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  "& > :not(style)": { m: 1 },
+                }}
+              >
+                <TextField
+                  helperText="Please enter your password"
+                  id="demo-helper-text-misaligned"
+                  label="Password"
+                />
+              </Box>
+            </div>
+          </Box>
+          <Button sx={{marginLeft :'30%' ,display : 'flex' , justifyContent : 'center' , alignItems : 'center'}} variant="contained" disableElevation>
+            Disable elevation
+          </Button>
         </div>
       </section>
     </>
